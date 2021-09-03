29a00540c482bb3a9ee5762e7c77c61045d2d595
Pierre Breny <pierre.breny@gmail.com>
2021-09-03 11:01:24 +0200

    #1 push on Heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// Database
+
+// Mongoose
+mongoose.connect(, {useNewUrlParser: true, useUnifiedTopology : true})
+
+// Routes
+
+app.listen(8080, () => {
+    console.log("Listening on 3000");
+}); 
