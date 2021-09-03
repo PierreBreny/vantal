53bfa8e0be3480b3e3dc340a5cc6d63c41892960
Pierre Breny <pierre.breny@gmail.com>
2021-09-03 11:09:33 +0200

    connected to mongo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 // Database
 
 // Mongoose
-mongoose.connect(, {useNewUrlParser: true, useUnifiedTopology : true})
+mongoose.connect("mongodb+srv://vantal:vantal123@cluster0.eu4l9.mongodb.net/vantal?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology : true})
 
 // Routes
 app.use("/", (req, res) => {
