15d66c1b86e30849a4370b6ae46c3a3be44c4629
Pierre Breny <pierre.breny@gmail.com>
2021-09-03 11:28:31 +0200

    added Procfile

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use("/", (req, res) => {
     res.send("This is working!")
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Listening on 3000");
 }); 
