243b0de259dcf216af507ed4cc55c451cf92625a
Pierre Breny <pierre.breny@gmail.com>
2021-09-06 14:32:28 +0200

    added login/register and it works locally

--- app.js
+++ app.js
@@ -15,12 +15,32 @@ dotenv.config();
 // Mongoose
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log("Succesfully connected to Mongo!"))
-.catch((err) => console.log(err))
+.catch((err) => console.log(err));
 
-// Routes
-app.use("/", (req, res) => {
-    res.send("This is working!")
-})
+//EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+//BodyParser
+app.use(express.urlencoded({extended : false}));
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req,res,next)=> {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error  = req.flash('error');
+    next();
+    })
+
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
 
 app.listen(process.env.PORT || 8080, () => {
     console.log("Listening on 8080");
