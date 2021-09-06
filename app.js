26c84a12b6c1e2c587084927534ef22ed7b3155c
Pierre Breny <pierre.breny@gmail.com>
2021-09-06 14:16:29 +0200

    succesfully connected to Mongo

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const expressEjsLayout = require("express-ejs-layouts");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport")
 const dotenv = require('dotenv');
 dotenv.config();
+
+// passport config
 
 // Database
 
 // Mongoose
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log("Succesfully connected to Mongo!"))
+.catch((err) => console.log(err))
 
 // Routes
 app.use("/", (req, res) => {
