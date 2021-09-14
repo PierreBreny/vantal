5eca4c77d153112a267005fd381d72a3aa4e1760
Pierre Breny <pierre.breny@gmail.com>
2021-09-14 16:53:21 +0200

    if user has listings, they are now displayed in /listings page. If user does not have listings, a 'create a listing' message is displayed instead.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
-const {ensureAuthenticated} = require('../config/auth') 
+const {ensureAuthenticated} = require('../config/auth'); 
+const Profile = require("../models/profile").Profile;
 //login page
 router.get('/', (req,res)=>{
     res.render('welcome');
@@ -10,9 +11,12 @@ router.get('/', (req,res)=>{
 router.get('/register', (req,res)=>{
     res.render('register');
 })
+
+//dashboard page with all listings rendered
 router.get('/dashboard',ensureAuthenticated,(req,res)=>{
     res.render('dashboard',{
-        user: req.user
-    });
+        user: req.user,
+    });;
 })
+
 module.exports = router; 
--- routes/listings.js
+++ routes/listings.js
@@ -4,18 +4,21 @@ const Listing = require("../models/listing").Listing;
 const Profile = require("../models/profile").Profile;
 const {ensureAuthenticated} = require('../config/auth'); 
 
-// Listings dashboard -> If user has none, page is empty and button redirects to 'listings/new'
-router.get('/', ensureAuthenticated, (req,res) =>{
-    res.render('listing', {
-        user: req.user
-    });
-})
+// // User listings dashboard -> If user has none, page is empty and button redirects to 'listings/new'
+// router.get('/', ensureAuthenticated, (req,res) =>{
+//     res.render('myListing', {
+//         user: req.user,
+//         profile: req.user.profile
+//     });
+// })
 
+// Create new listing page
 router.get('/new', ensureAuthenticated, (req,res) =>{
     res.render('newListing', {
         user: req.user
     });
 })
+
 
 //Create a new listing
 router.post('/new', ensureAuthenticated, async (req,res) => {
@@ -24,11 +27,31 @@ router.post('/new', ensureAuthenticated, async (req,res) => {
     try {
         await newListing.save()
         await Profile.findOneAndUpdate({_id: req.user.profile._id}, {$push: {listings: newListing._id}})
-        res.redirect('/listings/');
+        res.redirect('/dashboard/');
     } catch (error) {
         console.log(error);
         res.redirect('/listings/new');
     }
+})
+
+
+// Display all listings
+const getProfileAndPopulate = function(id){
+    return Profile.findById(id).populate('listings')
+}
+
+const renderListings = async function (req, res){
+    listings = await getProfileAndPopulate(req.user.profile._id)
+    console.log(listings)
+    res.render('myListing',{
+        user: req.user,
+        listings: listings
+    });
+}
+
+//dashboard page with all listings rendered
+router.get('/',ensureAuthenticated,(req,res)=>{
+    renderListings(req, res);
 })
 
 module.exports = router; 
